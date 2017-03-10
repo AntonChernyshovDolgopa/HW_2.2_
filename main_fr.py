fc64d4a730beade6fc545988a3231898d467884e
AntonChernyshovDolgopa <chernyshow59784@mail.ru>
2017-03-10 20:31:41 +0300

    Add files via upload

---
+++ main_fr.py
@@ -0,0 +1,21 @@
+import json
+import re
+from collections import Counter
+from html.parser import HTMLParser
+  
+pattern = r'\b[a-zA-Zа-яА-Я]{1}[a-zA-Zа-яА-Я\-]{5,}[a-zA-Zа-яА-Я]{1}\b'
+
+json_file = open('newsfr.json', encoding='ISO-8859-5')
+json_news = json.load(json_file)
+
+news =  json_news['rss']['channel']['item']
+words = []
+
+for news_item in news:
+	news_words = news_item['description']['__cdata']
+	suitable_words = re.findall(pattern, news_words)
+	words = words + suitable_words
+words_count = Counter(words)
+top10 = words_count.most_common(10)
+for item in top10:
+	print (item[0] + ', ' + '%d раз(а)' % (item[1]))	
